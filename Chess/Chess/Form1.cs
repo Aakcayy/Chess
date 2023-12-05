2da2ceba9a094542189536fef163cb3ee4d8fa4f
Ahmet Özdemir Akçay <ahmetakcay944@gmail.com>
2023-12-05 12:37:12 +0300

    3
    Kalıtım kullanıldı

--- Chess/Chess/Form1.cs
+++ Chess/Chess/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Chess.piece;
 
 namespace Chess
 {
@@ -18,14 +19,19 @@ namespace Chess
             InitializeComponent();
 
         }
-        bool isFirstClick = true;
+       
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            kale.kale(pictureBox1, pictureBox16, pictureBox2);
+            kale.kale1();
+            bool isFirstClick = true;
             Image tempImage; //Image sınıfından tempImage değişkeni
             if (isFirstClick)
             {
                 // İlk tıklama
-                kale.kale2();
+               
+                pictureBox2.BackColor = Color.Black;
+                pictureBox16.BackColor = Color.Black;
             }
             else
             {
@@ -49,25 +55,8 @@ namespace Chess
 
         private void pictureBox7_Click(object sender, EventArgs e)
         {
-            Image tempImage;
-            if (isFirstClick)
-            {
-                // İlk tıklama
-                pictureBox9.BackColor = Color.Black;
-                pictureBox8.BackColor = Color.Black;// 
-                MessageBox.Show("Önünde bir taş olmadığı takdirde ileriye, geriye, sola ve sağa doğru istenildiği kadar gidilebilir. ", "Kale", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
-            {
-                // İkinci tıklama
-                pictureBox9.BackColor = Color.Transparent;
-                pictureBox8.BackColor = Color.Transparent;
-                tempImage = pictureBox7.Image;
-                pictureBox7.Image = pictureBox8.Image;
-                pictureBox8.Image = tempImage;
-            }
-
-            isFirstClick = !isFirstClick;
+            kale.kale(pictureBox8, pictureBox9, pictureBox7);
+            kale.kale1();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -76,22 +65,51 @@ namespace Chess
         }
     
     }
-    public class castle
+    public class piece
     {
         Image tempImage;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox16;
-        public void kale(PictureBox pictureBox2, PictureBox pictureBox16)
-      {
-            this.pictureBox2 = pictureBox2;
-            this.pictureBox16 = pictureBox16;
-
-        }
-        public void kale2()
+        bool isFirstClick = true;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2, pictureBox3,pictureBox4, pictureBox5, pictureBox6, pictureBox10, pictureBox11, pictureBox12, pictureBox13, pictureBox14, pictureBox15, pictureBox16;
+       
+       
+        
+        public class castle:piece
         {
-            pictureBox2.BackColor = Color.Black;
-            pictureBox16.BackColor = Color.Black;
-           
+            public void kale(PictureBox pictureBox8, PictureBox pictureBox9, PictureBox pictureBox7)
+            {
+
+                this.pictureBox8 = pictureBox8;
+                this.pictureBox7 = pictureBox7;
+                this.pictureBox9 = pictureBox9;
+
+            }
+            public void kale1()
+            {
+                Image tempImage;
+                if (isFirstClick)
+                {
+                    // İlk tıklama
+                    pictureBox9.BackColor = Color.Black;
+                    pictureBox8.BackColor = Color.Black;// 
+                    MessageBox.Show("Önünde bir taş olmadığı takdirde ileriye, geriye, sola ve sağa doğru istenildiği kadar gidilebilir. ", "Kale", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else
+                {
+                    // İkinci tıklama
+                    pictureBox9.BackColor = Color.Transparent;
+                    pictureBox8.BackColor = Color.Transparent;
+                    tempImage = pictureBox7.Image;
+                    pictureBox7.Image = pictureBox8.Image;
+                    pictureBox8.Image = tempImage;
+                }
+
+                isFirstClick = !isFirstClick;
+            }
         }
 
     }
